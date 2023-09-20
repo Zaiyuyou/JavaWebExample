98aa7ef7b601e30a0010317bc02fb78b0ea75865
Zaiyuyou <zaiyuyou@foxmail.com>
2023-09-20 11:56:25 +0800

    Tomcat Servlet

--- src/main/java/com/example/StartDispatch.java
+++ src/main/java/com/example/StartDispatch.java
@@ -2,37 +2,49 @@ package com.example;
 import java.io.IOException;
 import java.io.PrintWriter;
 import javax.servlet.RequestDispatcher;
-import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-public class Servlet1 extends HttpServlet {
+public class StartDispatch extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
 
         //设置连接样式和字符编码格式
-        response.setContentType("text/html;charset=utf-8");//此
+        response.setContentType("text/html;charset=utf-8");
 
         //随便新建一个屏幕打印器并打印一些字符
         PrintWriter out = response.getWriter();
-        out.print("Hello,Http Get 1");
+        out.print("Hello,Dispatch Get 1");
 
         //通过request类方法新建请求转发器并转发请求
         RequestDispatcher rd = request.getRequestDispatcher("s2");
-        rd.forward(request, response);
+        rd.include(request, response);
 
         //通过ServletContext类方法新建请求转发器并转发请求
         RequestDispatcher sc = getServletContext().getRequestDispatcher("/s2");
-        sc.forward(request, response);
+        sc.include(request, response);
 
 
 
     }
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
+        //设置连接样式和字符编码格式
+        response.setContentType("text/html;charset=utf-8");
+
+        //随便新建一个屏幕打印器并打印一些字符
         PrintWriter out = response.getWriter();
-        out.print("Hello,Http Post");
+        out.print("Hello,Dispatch Post 1");
+
+        //通过request类方法新建请求转发器并转发请求
+        RequestDispatcher rd = request.getRequestDispatcher("s2");
+        rd.include(request, response);
+
+        //通过ServletContext类方法新建请求转发器并转发请求
+        RequestDispatcher sc = getServletContext().getRequestDispatcher("/s2");
+        sc.include(request, response);
     }
 }
--- src/main/java/com/example/StartRedirect.java
+++ src/main/java/com/example/StartRedirect.java
@@ -1,29 +1,41 @@
 package com.example;
-
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.IOException;
-import java.io.PrintWriter;
 
-public class Servlet2 extends HttpServlet {
+public class StartRedirect extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         //设置连接样式和字符编码格式
         response.setContentType("text/html;charset=utf-8");
 
         //随便新建一个屏幕打印器并打印一些字符
         PrintWriter out = response.getWriter();
-        out.print(" | Hello,Http Get 2");
+        out.print("Hello,Redirect Get 1");
+
+        //重定向get请求
+        response.sendRedirect("s2");
+
+
+
     }
     public void doPost(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
+
         //设置连接样式和字符编码格式
         response.setContentType("text/html;charset=utf-8");
 
         //随便新建一个屏幕打印器并打印一些字符
         PrintWriter out = response.getWriter();
-        out.print(" | Hello,Http Post 2");
+        out.print("Hello,Redirect Post 1");
+
+        //重定向Post请求
+        response.sendRedirect("s2");
+
     }
 }
